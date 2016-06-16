ff828d70c5038baf5bfdb1b88baa8772af06cacd
jim-clark <email@jim-clark.com>
2016-06-16 09:23:04 -0700

    Add payout info

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat');
+		gutil = require('gulp-util'),
+		uglify = require('gulp-uglify'),
+		concat = require('gulp-concat');
 
 gulp.task('js', function () {
-    gulp.src(['./public/app/*.js','./public/app/**/*.js'])
-        .pipe(uglify())
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./public/dist'));
+  gulp.src(['./public/app/*.js','./public/app/**/*.js'])
+    .pipe(uglify())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./public/dist'));
 });
 
 gulp.task('default', function () {
