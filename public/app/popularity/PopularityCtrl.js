ab7109c59ef45ceac079b7aa3c29072a21a34e73
jim-clark <email@jim-clark.com>
2016-07-28 09:21:18 -0700

    Fix: initialize popularity in PopularityCtrl.js

--- public/app/popularity/PopularityCtrl.js
+++ public/app/popularity/PopularityCtrl.js
@@ -5,9 +5,10 @@
   angular.module('app')
     .controller('PopularityCtrl', ['$rootScope', '$scope', 'ParticipantDataService', function($rootScope, $scope, ParticipantDataService) {
       
+      $scope.histogram = ParticipantDataService.histogram;
+
       $rootScope.$on('HISTOGRAM_UPDATED', function() {
         $scope.histogram = ParticipantDataService.histogram;
-        console.log($scope.histogram)
       });
 
     }
